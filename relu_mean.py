e9686fc52aac8e75a1b71bf59a30d257f73798b6
masataka <oomasatakamitsu@user-no-MacBook-Air.local>
2016-07-20 17:04:01 +0900

    add activation function about a value

--- relu_mean.py
+++ relu_mean.py
@@ -32,33 +32,34 @@ class ReLU_MEAN(function.Function):
 
     def forward_cpu(self, x):
         self.cpu_y = utils.force_array(numpy.maximum(x[0], 0, dtype=x[0].dtype))
-        self.cpu_y -= self.cpu_y.sum(axis=1, keepdims=True) / 4 # num_of_Actions
+        self.cpu_y -= self.cpu_y.sum(axis=1, keepdims=True) / 4. # num_of_Actions
         return self.cpu_y,
 
     def forward_gpu(self, x):
         if (cuda.cudnn_enabled and self.use_cudnn and
                 (_cudnn_version >= 3000 or x[0].dtype != numpy.float16)):
             gpu_y = cudnn.activation_forward(x[0], _mode)
-            gpu_y -= gpu_y.sum(axis=1, keepdims=True) / 4 # num_of_Actions
+            gpu_y -= gpu_y.sum(axis=1, keepdims=True) / 4. # num_of_Actions
             self.y = gpu_y
         else:
             gpu_y = cuda.cupy.maximum(x[0], 0)
-            gpu_y -= gpu_y.sum(axis=1, keepdims=True) / 4 # num_of_Actions
+            gpu_y_sum = gpu_y.sum(axis=1, keepdims=True) / 4. # num_of_Actions
+            gpu_y -= gpu_y_sum
         return gpu_y,
 
     def backward_cpu(self, x, gy):
-        cpu_gy = utils.force_array(gy[0] * (x[0] > 0) * (1 - 1 / 4)) # 4 is num_of_Actions
+        cpu_gy = utils.force_array(gy[0] * (x[0] > 0) * (1 - 1 / 4.)) # 4 is num_of_Actions
         return cpu_gy,
 
     def backward_gpu(self, x, gy):
         if (cuda.cudnn_enabled and self.use_cudnn and
                 (_cudnn_version >= 3000 or x[0].dtype != numpy.float16)):
             gx = cudnn.activation_backward(x[0], self.y, gy[0], _mode)
-            gx = gx * (1 - 1 / 4 ) # 4 is num_of_Actions
+            gx = gx * (1 - 1 / 4. ) # 4 is num_of_Actions
         else:
             gx = cuda.elementwise(
                 'T x, T gy', 'T gx',
-                'gx = x > 0 ? gy * (1 - 1 / 4) : (T)0',
+                'gx = x > 0 ? gy * (1 - 1 / 4.) : (T)0',
                 'relu_mean_bwd')(x[0], gy[0]) # 4 is num_of_Actions
         return gx,
 
